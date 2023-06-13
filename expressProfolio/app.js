b24667b0b8068dcaecbbfd94074862113f627587
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-12 21:00:31 -0400

    Update app.js

--- expressProfolio/app.js
+++ expressProfolio/app.js
@@ -2,6 +2,16 @@
 Student ID: 301276248
 Date:   06/04/2023
 Filename: app.js */
+require('dotenv').config();
+
+const mongoose = require('mongoose')    
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
