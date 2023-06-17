6cac39667b276ff5ed1ea688a442cb32a8b33892
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-17 16:59:15 -0400

    Update app.js

--- expressProfolio/app.js
+++ expressProfolio/app.js
@@ -5,12 +5,12 @@ Filename: app.js */
 require('dotenv').config()
 const mongoose = require('mongoose')  
 
-mongoose.connect(process.env.DATABASE_URL, { 
+/*mongoose.connect(process.env.DATABASE_URL, { 
   useNewUrlParser: true,
   useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+db.once('open', () => console.log('Connected to Database'))*/
 
 var createError = require('http-errors');
 var express = require('express');
