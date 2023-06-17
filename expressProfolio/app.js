fb890da8d3945dd9eb7ddbe35a839eb8f4239d20
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-17 16:43:01 -0400

    Update app.js

--- expressProfolio/app.js
+++ expressProfolio/app.js
@@ -5,7 +5,9 @@ Filename: app.js */
 require('dotenv').config()
 const mongoose = require('mongoose')  
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
