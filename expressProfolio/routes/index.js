b96e621eb60ebb4e073c2103c50cb00985d34704
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-18 16:38:09 -0400

    Update index.js

--- expressProfolio/routes/index.js
+++ expressProfolio/routes/index.js
@@ -32,9 +32,9 @@ router.get('/contact', function(req, res, next) {
 
 
 router.get('/business_contact', function(req, res, next) {
-  var contact_list = business_contact.find();
-  console.log(contact_list );
-  res.render("business_contact", { title: 'Business Contact', list: contact_list });
+  business_contact.find({}, function (err, list){
+  res.render('business_contact', { title: 'Business Contact', list: list});
+  });
 });
 /*
 router.get('/business_contact', (req,res,next)=> {
