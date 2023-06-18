611abcc0e6dc59f218e6f73b00cec978ec6a9f28
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-18 13:14:49 -0400

    Update app.js

--- expressProfolio/app.js
+++ expressProfolio/app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/busciness_contact'. contactlist);
+app.use('/contact-list'. contactlistRouter);
 
 //setup db
 require('dotenv').config();
