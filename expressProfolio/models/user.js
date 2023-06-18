343906c51a3cebe7c304b0cf32f5d3f2714edff6
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-18 14:56:40 -0400

    p

--- expressProfolio/models/user.js
+++ expressProfolio/models/user.js
@@ -1,4 +1,20 @@
-const mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+  
+  
+var UserSchema = new Schema({   
+    email: {type: String, required:true, unique:true},
+    username : {type: String, unique: true, required:true},
+});
+  
+// plugin for passport-local-mongoose
+UserSchema.plugin(passportLocalMongoose);
+  
+// export userschema
+ module.exports = mongoose.model("User", UserSchema);
+
+/*const mongoose = require('mongoose')
 var passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -14,5 +30,5 @@ const userSchema = new mongoose.Schema({
   }
 })
 userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)*/
 
--- expressProfolio/routes/users.js
+++ expressProfolio/routes/users.js
@@ -12,15 +12,48 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post("/register", function (req, res) {
-  User.register(new User({ email: req.body.email, name: req.body.name }), req.body.password, function (err, user) {
-      if (err) {
-          res.json({ success: false, message: "Your account could not be saved. Error: " + err });
-      }
-      else {
-        res.json({ success: true, message: "Your account has been saved" });
-      }
-  });
+router.post('/register', function (req, res) {
+	User.register(new User({ email: req.body.email, username: req.body.name }), req.body.password, function (err, user) {
+		if (err) {
+			res.json({ success: false, message: "Your account could not be saved. Error: " + err });
+		}
+		else {
+			req.login(user, (er) => {
+				if (er) {
+					res.json({ success: false, message: er });
+				}
+				else {
+					res.json({ success: true, message: "Your account has been saved" });
+				}
+			});
+		}
+	});
 });
+
+router.post("/login", function (req, res) {
+	if (!req.body.username) {
+		res.json({ success: false, message: "Username was not given" })
+	}
+	else if (!req.body.password) {
+		res.json({ success: false, message: "Password was not given" })
+	}
+	else {
+		passport.authenticate("local", function (err, user, info) {
+			if (err) {
+				res.json({ success: false, message: err });
+			}
+			else {
+				if (!user) {
+					res.json({ success: false, message: "username or password incorrect" });
+				}
+				else {
+					const token = jwt.sign({ userId: user._id, username: user.username }, secretkey, { expiresIn: "24h" });
+					res.json({ success: true, message: "Authentication successful", token: token });
+				}
+			}
+		})(req, res);
+	}
+});
+
 
 module.exports = router;
