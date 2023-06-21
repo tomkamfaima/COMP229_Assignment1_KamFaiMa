5df60925966e519f6cfa48266a2348ef845852e6
tomkamfaima <133718346+tomkamfaima@users.noreply.github.com>
2023-06-21 17:57:23 -0400

    test

--- expressProfolio/app.js
+++ expressProfolio/app.js
@@ -11,18 +11,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
+const jwt = require('jsonwebtoken')
+const jwtSecret  = "fef165c39c78bd62f649ea39c0ed24a9192659dcee3aa3ff5ae9ea3e7a4a71d670eaea";
+
 
 //for authenication
 const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
 
 
 //routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 //extract data from form
 app.use(express.urlencoded({extended:false}))
@@ -54,55 +54,6 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-
-app.get('/business_contact', checkAuthenticated, (req, res) => {
-  res.render('business_contact.ejs', { name: req.user.name })
-})
-
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport,
-  email => User.find(User => User.email === email),
-  id => User.find(User => User.id === id)
-)
-
-app.use(session({
-  secret: "SomeSecret",
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
-app.get('/login',  (req, res) => {
-  res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/business_contact',
-  failureRedirect: '/register',
-  failureFlash: true
-}))
-/*
-app.get('/register', (req, res) => {
-  res.render('register.ejs')
-})*/
-/*
-app.post('/register', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-      });
-  } catch (err){
-    console.log(err);
-  }
-})*/
-
 app.post("/register", async (req, res) =>{
   const hashedPassword = await bcrypt.hash(req.body.password,10);
   const user = new User({
@@ -112,42 +63,18 @@ app.post("/register", async (req, res) =>{
   })
     try {
       const newUser = await user.save()
-      res.status(201).json(newUser)
-      redirect('/business_contact');
+      res.redirect('/business_contact');
     } catch (err) {
       res.status(400).json({ message: err.message })
     }
 });
 
 
-app.post('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/login')
-}
 
 app.get('/delete/:id', async(req,res)=>{
   await Business_contact.deleteOne({_id: req.params.id});
   res.redirect('/business_contact');
 });
-app.get('/update/:id', async(req,res)=>{
-  await Business_contact.deleteOne({_id: req.params.id});
-  res.redirect('/update');
-});
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    console.log("success");
-    return res.redirect('/business_contact')
-  }
-  next()
-}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -159,10 +86,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 app.listen(3001);
 module.exports = app;
--- expressProfolio/routes/index.js
+++ expressProfolio/routes/index.js
@@ -9,73 +9,189 @@ const Business_contact = require('../models/business_contact')
 const User = require('../models/user');
 const { render } = require('ejs');
 
+const jwt = require('jsonwebtoken')
+const jwtSecret  = "fef165c39c78bd62f649ea39c0ed24a9192659dcee3aa3ff5ae9ea3e7a4a71d670eaea";
+var _inOut;
+
+
+function checkInOut(_req){
+  const token = _req.cookies.jwt;
+  if(token){
+    console.log(token);
+    jwt.verify(token, jwtSecret, (err,decodedToken) => {
+      if(err){
+        res.status(400).json({ message: err.message });
+      }else{
+        _inOut = "Log Out";
+      }
+    })
+  }else{
+    _inOut = "Log In";
+  }
+};
+
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  checkInOut(req);
+  res.render('index', { title: 'Home', inOut: _inOut });
 });
-
 /* GET About Me page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  checkInOut(req);
+  res.render('about', { title: 'About Me', inOut: _inOut });
 });
 /* GET Project page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  checkInOut(req);
+  res.render('projects', { title: 'Projects', inOut: _inOut });
 });
 /* GET Service page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+  checkInOut(req);
+  res.render('services', { title: 'Services', inOut: _inOut });
 });
 /* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  checkInOut(req);
+  res.render('contact', { title: 'Contact Me', inOut: _inOut });
 });
-
-router.get('/update', function(req, res, next) {
-  res.render('update', { title: 'Update' });
+router.get('/addBus', function(req, res, next) {
+    const token = req.cookies.jwt;
+    checkInOut(req);
+    if(!token){
+      res.render('login', { title: 'Login', inOut: _inOut });
+    }else{
+      jwt.verify(token, jwtSecret, (err,decodedToken) => {
+        if(err){
+          res.render('login', { title: 'Login', inOut: _inOut });
+        }else{
+          res.render('addBus', { title: 'Add New Business Contact', inOut: _inOut });
+        }
+      })
+    }
 });
-
 
 /* GET Login page. */
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+  const token = req.cookies.jwt;
+  checkInOut(req);
+  if(!token){
+  res.render('login', { title: 'Login', inOut: _inOut });}
+  else{
+    jwt.verify(token, jwtSecret, (err, decodedToken)=> {
+      if(err){
+        res.render('login', { title: 'Login', inOut: _inOut });
+      }else {
+        res.redirect('/business_contact');
+      }
+    })
+  }
 });
 /* GET Register page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+  checkInOut(req);
+  res.render('register', { title: 'Register' , inOut: _inOut});
 });
-/*
-router.post('/register', (req,res) =>{
-    const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-    });
-    newUser.save()
-});*/
 
-router.post('/update', async(req,res) =>{
-    const new_business_contact = new Business_contact({
-      name: req.body.name,
-      email: req.body.email,
-      contact_number: req.body.contact_number
-      });
-  new_business_contact.save();
+router.get('/business_contact', async(req,res,next)=>{
+  checkInOut(req);
   try{
-    const contacts = await Business_contact.find();
-    res.render('business_contact', { title: 'Business Contact', contacts:contacts });
-}catch (err){
+    const contacts = await Business_contact.find().sort("name");
+    const token = req.cookies.jwt;
+    if(!token){
+      res.render('login', { title: 'Login', inOut: _inOut });
+    }else{
+      jwt.verify(token, jwtSecret, (err,decodedToken) => {
+        if(err){
+          res.render('login', { title: 'Login', inOut: _inOut });
+        }else{
+          res.render('business_contact', { title: 'Business Contact', contacts:contacts , inOut: _inOut});
+        }
+      })
+    }
+  }catch{
+    res.status(500).json({message:err.message});
+  }
+})
+
+router.get('/update/:id', async(req,res)=>{
+  checkInOut(req);
+  try{
+  const user = await Business_contact.findOne({_id: req.params.id});
+  res.render('update', { title: 'Update', user:user , inOut: _inOut});
+  }catch(err) {
     res.status(500).json({message:err.message})
-}
-});
-
-router.get('/business_contact', async (req,res, next)=>{
-  try{
-      const contacts = await Business_contact.find();
-      res.render('business_contact', { title: 'Business Contact', contacts:contacts });
-  }catch (err){
-      res.status(500).json({message:err.message})
   }
 });
+
+router.post('/update', async(req,res) =>{
+  const filter = { email: req.body.email };
+  const update = {name: req.body.name, contact_number: req.body.contact_number};
+  try{
+    const user = await Business_contact.findOneAndUpdate(filter, update, {new: true});
+    res.redirect('/business_contact');
+  }catch(err){
+    res.status(500).json({message:err.message})
+  }
+});
+
+router.post('/addBus', async(req, res, next) => {
+  const new_business_contact = new Business_contact({
+    name: req.body.name,
+    email: req.body.email,
+    contact_number: req.body.contact_number
+    });
+    new_business_contact.save();
+    try{
+      res.redirect('/business_contact');
+    }catch (err){
+      res.status(500).json({message:err.message})
+    }
+});
+router.post('/login', async(req, res, next) => {
+    try{
+      const _email = req.body.email;
+      const user = await User.findOne({email:_email});
+      if(!user){
+        res.status(400).json({
+          messagae: "Login fails",
+          error: "User not found"
+        })}else{
+          bcrypt.compare(req.body.password, user.password, (err, data) => {
+            if (err){
+              res.status(400).json({
+                message: "Error",
+                error: err.messagae})
+            }
+            if (data) {
+              const maxAge = 3600;
+              const token = jwt.sign({
+                id:user._id, email: _email}, jwtSecret,{expiresIn: maxAge}
+              );
+              res.cookie("jwt", token, {
+                httpOnly: true,
+                maxAge: maxAge*1000
+              });
+                res.redirect('/business_contact');
+            } else {
+                res.redirect('/login');
+            }
+        })
+    }}catch (err){
+      res.status(400).json({
+        message: "Error",
+        error: err.messagae})
+    }
+});
+
+router.get("/logout", (req, res) => {
+  const token = req.cookies.jwt;
+  if(!token){
+    res.redirect("/login")}
+  else{
+    res.cookie("jwt", "", { maxAge: "0" })
+    res.redirect("/")
+  }
+})
 
 module.exports = router;
